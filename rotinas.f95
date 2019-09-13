dd1e9bcc7aa637a1ae068707f35acc50e2cf92c6
gdavid91 <gustavoquintero9105@gmail.com>
2019-09-13 10:59:59 -0300

    Agregando types a usar

--- rotinas.f95
+++ rotinas.f95
@@ -1,3 +1,27 @@
 Module Tools
     implicit none
+    
+    type PackedVector
+        integer :: NZ, NFull
+        real, allocatable :: Value(:)
+        integer, allocatable :: VectorIndex(:)
+    end type PackedVector
+
+    type PackedMatrixEntry
+        integer :: NNZ
+        integer, allocatable :: IRN(:), JCN(:)
+        real, allocatable :: Value(:)
+    end type PackedMatrixEntry
+    
+    type PackedMatrixCol
+        integer, allocatable :: LenCol(:), ColStart(:), RowIndex(:)
+        real, allocatable :: Value(:)
+    end type PackedMatrixCol
+
+    type PackedMatrixRow
+        integer, allocatable :: LenRow(:), RowStart(:), ColIndex(:)
+        real, allocatable :: Value(:)
+    end type PackedMatrixRow
+    
+    
 end Module Tools
