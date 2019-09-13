5a2ddccca06ac570c86c357d1131e1a223c6592b
copacondo <copacondodaniel@gmail.com>
2019-09-13 11:28:23 -0300

    primer modulo

--- daniel.f95
+++ daniel.f95
@@ -1,3 +1,28 @@
 program daniel
-
+	implicit none
+	!TYPE QUE EMPACOTA UM VETOR ESPARSO
+    type PackedVector
+        integer :: nz, NFull
+        real, allocatable :: Value(:)
+        integer, allocatable :: Vector_Index(:)
+    end type PackedVector
+    
+    !TYPE QUE EMPACOTA UMA MATRIZ NO ESQUEMA COORDENADAS
+    type PackedMatrixEntry
+        integer :: NNZ
+        integer, allocatable :: Row_Index(:), Col_Index(:)
+        real, allocatable :: Value(:)
+    end type PackedMatrixEntry
+    
+    !TYPE QUE EMPACOTA UMA MATRIZ COMO COLECAO DE COLUNAS
+    type PackedMatrixCol
+        integer, allocatable :: Len_Col(:), Col_Start(:), Row_Index(:)
+        real, allocatable :: Value(:)
+    end type PackedMatrixCol
+    
+    !TYPE QUE EMPACOTA UMA MATRIZ COMO COLECAO DE LINHAS
+    type PackedMatrixRow
+        integer, allocatable :: Len_Row(:), Row_Start(:), Col_Index(:)
+        real, allocatable :: Value(:)
+    end type PackedMatrixRow
 end program
