a6d051b9ae2550ac875757fbffecda680b9a71bc
gdavid91 <gustavoquintero9105@gmail.com>
2019-09-13 11:14:17 -0300

    Agregando types a usar

--- rotinas.f95
+++ rotinas.f95
@@ -3,27 +3,27 @@ module Tools
     
     !TYPE QUE EMPACOTA UM VETOR ESPARSO
     type PackedVector
-        integer :: NZ, NFull
+        integer :: nz, NFull
         real, allocatable :: Value(:)
-        integer, allocatable :: VectorIndex(:)
+        integer, allocatable :: Vector_Index(:)
     end type PackedVector
     
     !TYPE QUE EMPACOTA UMA MATRIZ NO ESQUEMA COORDENADAS
     type PackedMatrixEntry
         integer :: NNZ
-        integer, allocatable :: RowIndex(:), ColIndex(:)
+        integer, allocatable :: Row_Index(:), Col_Index(:)
         real, allocatable :: Value(:)
     end type PackedMatrixEntry
     
     !TYPE QUE EMPACOTA UMA MATRIZ COMO COLECAO DE COLUNAS
     type PackedMatrixCol
-        integer, allocatable :: LenCol(:), ColStart(:), RowIndex(:)
+        integer, allocatable :: Len_Col(:), Col_Start(:), Row_Index(:)
         real, allocatable :: Value(:)
     end type PackedMatrixCol
     
     !TYPE QUE EMPACOTA UMA MATRIZ COMO COLECAO DE LINHAS
     type PackedMatrixRow
-        integer, allocatable :: LenRow(:), RowStart(:), ColIndex(:)
+        integer, allocatable :: Len_Row(:), Row_Start(:), Col_Index(:)
         real, allocatable :: Value(:)
     end type PackedMatrixRow
     
