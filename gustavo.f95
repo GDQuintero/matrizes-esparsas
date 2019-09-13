4a11d3795b74c0d1cc74ff108d3180a3fb1b0712
gdavid91 <gustavoquintero9105@gmail.com>
2019-09-13 12:33:43 -0300

    Agregando funciones al modulo gustavo

--- gustavo.f95
+++ gustavo.f95
@@ -55,4 +55,65 @@ module Gustavo
             endif 
         enddo
     end function Gather
+    
+    !================================================================================================
+    ! FUNCAO PARA EMPACOTAR UMA MATRIZ ESPARSA (POR COLUNAS)
+    !================================================================================================
+    function GatherCol(FullMatrix)
+        implicit none
+        
+        integer :: i, j, k, Density, m, n, NonZero
+        real :: FullMatrix(:,:)
+        type(ColPacked) :: GatherCol
+        
+        m = size(FullMatrix(:,1)); n = size(FullMatrix(1,:)); Density = int(m*n*0.5)+1; NonZero = 0; k = 0
+        allocate(GatherCol%Len_Col(n),GatherCol%Col_Start(n))
+        allocate(GatherCol%Row_Index(Density),GatherCol%Value(Density))
+        
+        do j = 1, n
+            GatherCol%Col_Start(j) = 1 + k
+            do i = 1,m
+                if (FullMatrix(i,j) .ne. 0) then
+                    k = k + 1
+                    GatherCol%Row_Index(k) = i
+                    GatherCol%Value(k) = FullMatrix(i,j)
+                    NonZero = NonZero + 1
+                endif
+            enddo
+            GatherCol%Len_Col(j) = NonZero
+            NonZero = 0
+        enddo
+    end function GatherCol
+    
+    !================================================================================================
+    ! FUNCAO PARA EMPACOTAR UMA MATRIZ ESPARSA (POR LINHAS)
+    !================================================================================================
+    function GatherRow(FullMatrix)
+        implicit none
+        
+        integer :: i, j, k, Density, m, n, NonZero
+        real :: FullMatrix(:,:)
+        type(RowPacked) :: GatherRow
+        
+        m = size(FullMatrix(:,1)); n = size(FullMatrix(1,:)); Density = int(m*n*0.5)+1; NonZero = 0; k = 0
+        allocate(GatherRow%Len_Row(n),GatherRow%Row_Start(n))
+        allocate(GatherRow%Col_Index(Density),GatherRow%Value(Density))
+        
+        do i = 1, m
+            GatherRow%Row_Start(i) = 1 + k
+            do j = 1,n
+                if (FullMatrix(i,j) .ne. 0) then
+                    k = k + 1
+                    GatherRow%Col_Index(k) = j 
+                    GatherRow%Value(k) = FullMatrix(i,j)
+                    NonZero = NonZero + 1
+                endif
+            enddo
+            GatherRow%Len_Row(i) = NonZero
+            NonZero = 0
+        enddo
+    end function GatherRow
+    
+    
+    
 end module
